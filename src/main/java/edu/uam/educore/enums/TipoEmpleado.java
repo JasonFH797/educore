00061114181f615c46857eec7349abc7185ba0d1
JasonFH797 <jasonrolandofh@gmail.com>
2026-06-29 20:06:46 -0600

    feat: crear enum TipoEmpleado

---
+++ src/main/java/edu/uam/educore/enums/TipoEmpleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.uam.educore.enums;
+
+/**
+ *
+ * @author jason
+ */
+public enum TipoEmpleado {
+    DOCENTE,
+    ADMINISTRATIVO,
+    MANTENIMIENTO
+}
